c4cb19c0abebd6f30c948848908a608ae4bc389e
Taylor Noj <taylornoj@gmail.com>
2022-01-09 17:55:34 +0000

    add notes describing test methods

--- boat_spec.rb
+++ boat_spec.rb
@@ -5,7 +5,12 @@ require_relative 'boat'
 # all tests will go between do and end
 describe Boat do
   # it takes in string, which is description of feature to test
+  # here we are testing that Boat class can create instances
   it 'should create boats' do
+    #trying to asser that return value from Boat.new meets criteria, so Boat.new is parameter to expect
+    # 'to' method on obj returned from our call
+    # be_a = matcher; methods that can check whether argument passed expect
+    # then passing Boat class to be_a matcher; returning t/f
     expect(Boat.new).to be_a Boat
   end
 end
