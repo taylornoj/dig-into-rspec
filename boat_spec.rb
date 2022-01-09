4895366e45c764534e1281bf9127bdf3dc2434cf
Taylor Noj <taylornoj@gmail.com>
2022-01-09 17:49:13 +0000

    set up testing stage

--- boat_spec.rb
+++ boat_spec.rb
@@ -1,2 +1,11 @@
 # requiring code to test
 require_relative 'boat'
+
+# describe Boat = telling rspec what code you're writing tests for
+# all tests will go between do and end
+describe Boat do
+  # it takes in string, which is description of feature to test
+  it 'should create boats' do
+    expect(Boat.new).to be_a Boat
+  end
+end
